c592723420ec40ed0969189627a1fa186fd82f88
Swasti Agarwal <swasagr@gmail.com>
2020-10-17 19:16:53 +0200

    added more options and styling

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 
+/**
+ * function to calculate result
+ */
 function Calculate() {
-	var number1 = parseInt(document.getElementById("number1").value);
-	var number2 = parseInt(document.getElementById("number2").value);
+	// get both inputs
+	let number1 = parseInt(document.getElementById("number1").value);
+	let number2 = parseInt(document.getElementById("number2").value);
+
+	// check values and show error if no value was selected
 	if (isNaN(number1) || isNaN(number2)) {
-		alert("Wrong characters");
-		return false;
+		if (!document.getElementById("error")) {
+			var para = document.createElement("p");
+			para.setAttribute("id", "error");
+			para.innerHTML = "Please enter both numbers";
+			document.getElementById("calculator").appendChild(para);
+		}
+	} else {
+		if (document.getElementById("error")) document.getElementById("error").remove();
 	}
 
+	// get operator
 	var operator = document.getElementById("operator");
 	var selectedOperator = operator.options[operator.selectedIndex].value;
-
-	switch(selectedOperator) {
-		case "+" :
-			var result = number1 + number2;
+	let result = 0;
+	switch (selectedOperator) {
+		case "+":
+			result = number1 + number2;
 			break;
-		case "-" :
-			var result = number1 - number2;
-			break;	
-		case "*" :
-			var result = number1 * number2;
-			break;	
-		case "/" :
-			var result = number1 / number2;
+		case "-":
+			result = number1 - number2;
+			break;
+		case "*":
+			result = number1 * number2;
+			break;
+		case "/":
+			result = (number1 / number2).toFixed(4);
 			break;
 	}
-	document.getElementById("answer").value = result;    
+	document.getElementById("answer").value = result;
 }
