ec4896b9bf4b7a93ee54c421c306e8c20ec00a99
Swasti Agarwal <swasagr@gmail.com>
2020-10-17 13:23:32 +0200

    added logic for more operations

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-var number1 = document.getElementById("number1");
-var number2 = document.getElementById("number2");
 
 function Calculate() {
-	var number1_parsed = parseInt(number1.value);
-	var number2_parsed = parseInt(number2.value);
-	if (isNaN(number1_parsed) || isNaN(number2_parsed)) {
+	var number1 = parseInt(document.getElementById("number1").value);
+	var number2 = parseInt(document.getElementById("number2").value);
+	if (isNaN(number1) || isNaN(number2)) {
 		alert("Wrong characters");
 		return false;
 	}
 
-	var sum = number1_parsed + number2_parsed;
-	alert("Sum: " + sum);
+	var operator = document.getElementById("operator");
+	var selectedOperator = operator.options[operator.selectedIndex].value;
+
+	switch(selectedOperator) {
+		case "+" :
+			var result = number1 + number2;
+			break;
+		case "-" :
+			var result = number1 - number2;
+			break;	
+		case "*" :
+			var result = number1 * number2;
+			break;	
+		case "/" :
+			var result = number1 / number2;
+			break;
+	}
+	document.getElementById("answer").value = result;    
 }
